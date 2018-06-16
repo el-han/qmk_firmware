7e3d134919936860652e2bd8184b68de7278f091
Hannes Ellinger <hannes.ellinger@eas.iis.fraunhofer.de>
2018-06-16 10:56:35 +0200

    fix layers

--- keyboards/blackwing/keymaps/windows/keymap.c
+++ keyboards/blackwing/keymaps/windows/keymap.c
@@ -3,15 +3,19 @@
 
 enum blackwing_layers
 {
-  _STD,
+  _L1,
   _L3,
-  _L4
+  _L4,
+  _Q1,
+  _Q4
 };
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 #define KC_MU ALGR(DE_M)
+
+#define LEADER_TIMEOUT 300
 
 enum custom_keycodes {
     SLS_BSL = SAFE_RANGE,
@@ -36,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------+--------+--------+--------+--------+--------+--------''--------+--------+--------+--------+--------+--------+--------'
  */
 
-[_STD] = {
+[_L1] = {
   {KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    XXXXXXX, XXXXXXX, DE_6,    DE_7,    DE_8,    DE_9,    DE_0,     KC_BSPC},
   {KC_TAB,  DE_MINS, DE_COMM, DE_DOT,  DE_P,    DE_Y,    XXXXXXX, XXXXXXX, DE_F,    DE_G,    DE_C,    DE_T,    DE_Z,     KC_LEAD},
   {MO(_L3), DE_A,    DE_O,    DE_E,    DE_I,    DE_U,    XXXXXXX, XXXXXXX, DE_H,    DE_D,    DE_R,    DE_N,    DE_S,     MO(_L3)},
@@ -85,7 +89,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, KC_PGUP, KC_BSPC, KC_UP,   KC_DEL,  KC_PGDN, XXXXXXX, XXXXXXX, KC_PMNS, KC_7,    KC_8,    KC_9,    KC_PPLS, _______},
   {_______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX, XXXXXXX, KC_PSLS, KC_4,    KC_5,    KC_6,    KC_PAST, _______},
   {_______, KC_MUTE, KC_VOLD, KC_VOLU, KC_ENT,  KC_WHOM, XXXXXXX, XXXXXXX, KC_PEQL, KC_1,    KC_2,    KC_3,    DE_PERC, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______}
+  {DF(_Q1), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______}
+},
+
+/* QWERTY
+ * .-----------------------------------------------------.                  .-----------------------------------------------------.
+ * | ESC    | 1      | 2      | 3      | 4      | 5      |                  | 6      | 7      | 8      | 9      | 0      | ß      |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * | +      | Q      | W      | E      | R      | T      |                  | Z      | U      | I      | O      | P      | Ü      |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * | #      | A      | S      | D      | F      | G      |                  | J      | H      | K      | L      | Ö      | Ä      |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * | SHIFT  | Y      | X      | C      | V      | B      |                  | N      | M      | ,      | .      | -      | SHIFT  |
+ * |--------+--------+--------+--------+--------+--------+--------..--------+--------+--------+--------+--------+--------+--------|
+ * | CTRL   | META   | ALT    | BSPACE | SPACE  | SHIFT  | TAB    || ENTER  | SHIFT  | SPACE  | DEL    | ALTGR  | META   | CTRL   |
+ * '--------+--------+--------+--------+--------+--------+--------''--------+--------+--------+--------+--------+--------+--------'
+ */
+
+[_Q1] = { /* Standard; as compatible with dvorak and qwerty as possible */
+  {KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    XXXXXXX, XXXXXXX, DE_6,    DE_7,    DE_8,    DE_9,    DE_0,     DE_SS  },
+  {DE_PLUS, DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    XXXXXXX, XXXXXXX, DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,     DE_UE  },
+  {DE_HASH, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    XXXXXXX, XXXXXXX, DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,    DE_AE  },
+  {KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    XXXXXXX, XXXXXXX, DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS,  KC_RSFT},
+  {KC_LCTL, KC_LGUI, KC_LALT, MO(_Q4), KC_SPC,  KC_LSFT, KC_TAB,  KC_ENT,  KC_RSFT, KC_SPC,  MO(_Q4), KC_RALT, KC_RGUI,  KC_RCTL}
+},
+
+/*
+ * .-----------------------------------------------------.                  .-----------------------------------------------------.
+ * | F1     | F2     | F3     | F4     | F5     | F6     |                  | F7     | F8     | F9     | F10    | F11    | F12    |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * |        | PGUP   | BACKSP | UP     | DEL    | PGDOWN |                  | -      | 7      | 8      | 9      | +      |        |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * |        | HOME   | LEFT   | DOWN   | RIGHT  | END    |                  | /      | 3      | 5      | 6      | *      |        |
+ * |--------+--------+--------+--------+--------+--------|                  |--------+--------+--------+--------+--------+--------|
+ * |        | MUTE   | VOLDN  | VOLUP  | ENTER  | WHOME  |                  | =      | 1      | 2      | 3      | %      |        |
+ * |--------+--------+--------+--------+--------+--------+--------..--------+--------+--------+--------+--------+--------+--------|
+ * |        |        |        |        |        |        |        ||        |        |        | 0      |        |        |        |
+ * '--------+--------+--------+--------+--------+--------+--------''--------+--------+--------+--------+--------+--------+--------'
+ */
+
+[_Q4] = {
+  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
+  {_______, KC_PGUP, KC_BSPC, KC_UP,   KC_DEL,  KC_PGDN, XXXXXXX, XXXXXXX, KC_PMNS, KC_7,    KC_8,    KC_9,    KC_PPLS, _______},
+  {_______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX, XXXXXXX, KC_PSLS, KC_4,    KC_5,    KC_6,    KC_PAST, _______},
+  {_______, KC_MUTE, KC_VOLD, KC_VOLU, KC_ENT,  KC_WHOM, XXXXXXX, XXXXXXX, KC_PEQL, KC_1,    KC_2,    KC_3,    DE_PERC, _______},
+  {DF(_L1), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______}
 },
 
 /* blank key layout template
@@ -135,7 +183,7 @@ void matrix_scan_user(void)
   uint8_t layer = biton32(layer_state);
 
   switch (layer) {
-  case _STD:
+  case _L1:
       if (isShiftPressed) {
         set_led_green;
       } else {
@@ -146,7 +194,10 @@ void matrix_scan_user(void)
       set_led_cyan;
       break;
   case _L4:
-      set_led_red;
+      set_led_white;
+      break;
+  case _Q4:
+      set_led_white;
       break;
   default:
       break;
